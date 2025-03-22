9781a6504023646b945c9e9999c430cce25e21c2
Armen Matoosian <armen.matoosian@outlook.com>
2025-03-22 15:12:29 -0400

    changes to main.py for Day 18

--- Day 18/main.py
+++ Day 18/main.py
@@ -8,11 +8,27 @@ tim = Turtle()
 
 colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
+# # solution code specific to challenge 3 - Drawing Different Shapes
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         tim.forward(100)
+#         tim.right(angle)
+#
+# for shape_side_n in range(3, 11):
+#     tim.color(random.choice(colours))
+#     draw_shape(shape_side_n)
+
+# my code for challenge 4 - Generate a Random Walk
+
+
+
+
+
+
+
+
+
 
 for shape_side_n in range(3, 11):
     tim.color(random.choice(colours))
